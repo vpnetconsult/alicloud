c8e2d6b89ccad7d402761d05419c69443f058110
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 16:34:43 +0800

    Update alicloud-vpc.tf

--- alicloud-vpc.tf
+++ alicloud-vpc.tf
@@ -1,13 +1,13 @@
-resource "alicloud_vpc" "default" {
+resource "alicloud_vpc" "vpc" {
   vpc_name       = "tcloud-resource"
   cidr_block = "172.16.0.0/12"
 }
 
-resource "alicloud_vswitch" "default" {
+resource "alicloud_vswitch" "vswitch" {
   # Use the VPC's ID
-  vpc_id            = alicloud_vpc.default.id
+  vpc_id            = alicloud_vpc.vpc.id
   # Set the CIDR for this switch, must be in the CIDR of the VPC
-  cidr_block        = "172.16.0.0/12"
+  cidr_block        = "172.16.0.0/24"
   # As the VPC is a region-specified resource, switches are for zones
   zone_id = local.zone
 }
