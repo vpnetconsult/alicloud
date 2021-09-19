699aee28dbacc23920079158d6a73716cf9ecef0
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 16:23:02 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ terraform {
 provider "alicloud" {
   region = var.alicloud_region
 }
+
+# Some useful variables to reduce copy-paste, you can add whatever you like
+locals {
+  prefix   = "worker"
+  domain   = "vpnet.app"
+  hostname = "${local.prefix}.${local.domain}"
+  zone     = "eu-central-1"
+}
