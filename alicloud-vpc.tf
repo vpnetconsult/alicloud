e9412ecaba3e6789d15dd58a00161fd8973b6f78
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 16:20:20 +0800

    Update alicloud-vpc.tf

--- alicloud-vpc.tf
+++ alicloud-vpc.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    alicloud = {
-      source = "hashicorp/alicloud"
-      version = "1.135.0"
-    }
-  }
-}
-
-resource "alicloud_vpc" "vpc" {
+resource "alicloud_vpc" "default" {
   vpc_name       = "tcloud-resource"
   cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "default" {
+  # Use the VPC's ID
+  vpc_id            = alicloud_vpc.default.id
+  # Set the CIDR for this switch, must be in the CIDR of the VPC
+  cidr_block        = "172.16.0.0/12"
+  # As the VPC is a region-specified resource, switches are for zones
+  availability_zone = region
 }
 
