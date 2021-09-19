d4864e05ce8df6c840caab1372be1ecf7064e975
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 17:20:07 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+    alicloud = {
+      source = "hashicorp/alicloud"
+      version = "1.135.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
