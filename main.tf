8d9cde293e927fd7d35fe1cd4c0140ff8fcd02ca
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 16:16:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source = "hashicorp/alicloud"
+      version = "1.135.0"
+    }
+  }
+}
+
 provider "alicloud" {
   region = var.alicloud_region
 }
