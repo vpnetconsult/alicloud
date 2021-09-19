dc712954bc8e12e3cd34712fb5b6c9449a8eca63
eoskl <42143496+eoskl@users.noreply.github.com>
2021-09-19 14:17:40 +0800

    Update alicloud-vpc.tf

--- alicloud-vpc.tf
+++ alicloud-vpc.tf
@@ -11,3 +11,10 @@ resource "alicloud_vpc" "vpc" {
   name       = "tcloud-resource"
   cidr_block = "172.16.0.0/12"
 }
+
+resource "alicloud_vswitch" "vsw-web" {
+  name              = "subnet-tcloud"
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0./12"
+  availability_zone = "eu-central-1"
+}
