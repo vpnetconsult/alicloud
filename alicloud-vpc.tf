12abbd8d1c41bd4a86dd4c60b9c271014b27f06c
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-08 17:55:27 +0800

    Update alicloud-vpc.tf

--- alicloud-vpc.tf
+++ alicloud-vpc.tf
@@ -27,7 +27,7 @@ resource "alicloud_cs_serverless_kubernetes" "serverless" {
   deletion_protection            = false
   load_balancer_spec             = "slb.s2.small"
   time_zone                      = "Asia/Shanghai" 
-  service_cidr                   = "172.21.0.0/20"
+  service_cidr                   = "172.16.0.0/20"
   service_discovery_types        = ["PrivateZone"]
 }
 
