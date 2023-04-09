8203043916c66897100e262060587daef614a59c
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 10:48:31 +0800

    Update alicloud-vpc.tf

--- alicloud-vpc.tf
+++ alicloud-vpc.tf
@@ -17,20 +17,6 @@ resource "alicloud_vswitch" "vswitch" {
   zone_id = local.zone
 }
 
-resource "alicloud_cs_serverless_kubernetes" "serverless" {
-  name                           = "vpnet_serverless"
-  version                        = "1.24.6-aliyun.1"
-  vpc_id                         = alicloud_vpc.vpc.id
-  vswitch_ids                    = [alicloud_vswitch.vswitch.id]
-  new_nat_gateway                = true
-  endpoint_public_access_enabled = true
-  deletion_protection            = false
-  load_balancer_spec             = "slb.s2.small"
-  time_zone                      = "Asia/Shanghai" 
-  service_cidr                   = "10.1.0.0/20"
-  service_discovery_types        = ["PrivateZone"]
-}
-
 output "subnet_cidr_blocks" {
   description = "The vswitch cidr block"
   value = alicloud_vswitch.vswitch.*.cidr_block
