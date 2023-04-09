ecf75d7a992556552eaad5334f22a0fb690d3258
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 11:44:50 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,36 +1,48 @@
+
+# Create an API Gateway instance
+resource "alicloud_api_gateway" "vpnet_api_gateway" {
+  name = "vpnet-api-gateway"
+
+  # Define the protocol for the API Gateway instance
+  protocol = "https"
+
+  # Define the domain for the API Gateway instance
+  domain = "vpnet.app"
+
+  # Define the routing configuration for the API Gateway instance
+  route {
+    path        = "/my-endpoint"
+    service_id  = alicloud_fc_service.serverless_service.id
+    function_name = alicloud_fc_function.serverless_function.name
+    method      = "GET"
+    backend_type = "FC"
+    request_config {
+      request_protocol = "HTTP"
+    }
+    service_config {
+      vpc_id      = alicloud_vpc.vpc.id
+      vswitch_id  = alicloud_vswitch.vswitch.id
+    }
+  }
+}
+
 # Create a Serverless Function Compute service
 resource "alicloud_fc_service" "serverless_service" {
   name        = "vpnet-serverless-service"
   description = "My Serverless Service"
-  region      = local.zone
-  vpc_id      = alicloud_vpc.vpc.id
 }
 
 # Create a Serverless Function Compute function
 resource "alicloud_fc_function" "serverless_function" {
-  service_name = alicloud_fc_service.serverless_service.name
-  name         = "vpnet-serverless-function"
-  description  = "My Serverless Function"
-  runtime      = "nodejs12"
-  handler      = "index.handler"
-  code_filename = "serverless.zip"
-  memory_size  = 128
-  timeout      = 60
-}
+  service_name  = alicloud_fc_service.serverless_service.name
+  name          = "vpnet-serverless-function"
+  description   = "My Serverless Function"
+  runtime       = "nodejs12"
+  handler       = "index.handler"
+  memory_size   = 128
+  timeout       = 60
 
-# Create an API Gateway resource for routing HTTP requests to the function
-resource "alicloud_api_gateway" "api_gateway" {
-  name = "vpnet-api-gateway"
-  protocol = "HTTP"
-  instance_id = alicloud_fc_service.serverless_service.id
-  description = "My API Gateway"
-  api_group_name = "vpnet-api-group"
-  api_name = "vpnet-api"
-  method = "GET"
-  request_path = "/vpnet-api"
-}
-
-# Output the API Gateway URL
-output "api_gateway_url" {
-  value = alicloud_api_gateway.api_gateway.invoke_url
+  code {
+    zip_file = "path/to/serverless.zip"
+  }
 }
