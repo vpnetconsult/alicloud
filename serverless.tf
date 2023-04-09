d560a642bf532d742c042a1d1554f67aa9414d68
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 13:02:19 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,48 +1,20 @@
 
-# Create an API Gateway instance
-resource "alicloud_api_gateway" "vpnet_api_gateway" {
-  name = "vpnet-api-gateway"
+resource "alicloud_fc_service" "vpnet_service" {
+  service_name = "vpnet_service"
+  description  = "My Function Compute service"
+}
 
-  # Define the protocol for the API Gateway instance
-  protocol = "https"
-
-  # Define the domain for the API Gateway instance
-  domain = "vpnet.app"
-
-  # Define the routing configuration for the API Gateway instance
-  route {
-    path        = "/my-endpoint"
-    service_id  = alicloud_fc_service.serverless_service.id
-    function_name = alicloud_fc_function.serverless_function.name
-    method      = "GET"
-    backend_type = "FC"
-    request_config {
-      request_protocol = "HTTP"
-    }
-    service_config {
-      vpc_id      = alicloud_vpc.vpc.id
-      vswitch_id  = alicloud_vswitch.vswitch.id
-    }
+resource "alicloud_fc_function" "vpnet_function" {
+  function_name    = "vpnet_function"
+  service_name     = alicloud_fc_service.my_service.service_name
+  handler          = "index.handler"
+  runtime          = "nodejs10"
+  memory_size      = 128
+  timeout          = 3
+  code_file        = "path/to/your/function/code"
+  code_filename    = "index.js"
+  environment_vars = {
+    API_URL = "https://vpnet.app/api"
   }
 }
 
-# Create a Serverless Function Compute service
-resource "alicloud_fc_service" "serverless_service" {
-  name        = "vpnet-serverless-service"
-  description = "My Serverless Service"
-}
-
-# Create a Serverless Function Compute function
-resource "alicloud_fc_function" "serverless_function" {
-  service_name  = alicloud_fc_service.serverless_service.name
-  name          = "vpnet-serverless-function"
-  description   = "My Serverless Function"
-  runtime       = "nodejs12"
-  handler       = "index.handler"
-  memory_size   = 128
-  timeout       = 60
-
-  code {
-    zip_file = "path/to/serverless.zip"
-  }
-}
