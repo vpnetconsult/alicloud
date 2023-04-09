787723b0ac51cfc0d134d418e4024ef371bfc430
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 11:17:32 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,19 +1,32 @@
-# Create a Serverless Kubernetes cluster
-resource "alicloud_cs_serverless_kubernetes_cluster" "vpnet_serverless" {
-  name             = "vpnet_serverless"
-  vswitch_id       = alicloud_vswitch.vswitch.id
-  nat_gateway      = true
-  kubernetes_version = "1.21.2-aliyun.1"
-  num_of_nodes     = 3
-  region_id        = local.zone
+# Use the kubernetes module to create a serverless Kubernetes cluster
+module "vpnet_serverless" {
+  source  = "terraform-alicloud-modules/kubernetes/alicloud"
+  version = "3.3.0"
+
+  cluster_name            = "vpnet_serverless"
+  region_id               = local.zone
+  zone_ids                = [local.zone]
+  vpc_id                  = alicloud_vpc.vpc.id
+  vswitch_ids             = [alicloud_vswitch.vswitch.id]
+  nat_gateway             = true
+  num_of_nodes            = 3
+  kubernetes_version      = "1.21.2-aliyun.1"
+  enable_auto_renew       = true
+  enable_dashboard        = true
+  enable_nginx_ingress    = true
+  enable_log_service      = true
+  enable_private_zone     = true
+  enable_private_endpoint = true
+  tags                    = {
+    Environment = "production"
+  }
 }
 
-
-# Output the Kubernetes cluster details
+# Output the Kubernetes cluster and Load Balancer details
 output "cluster_details" {
   value = {
-    api_server_endpoint = alicloud_cs_serverless_kubernetes_cluster.vpnet_serverless.api_server_endpoint
-    cluster_id          = alicloud_cs_serverless_kubernetes_cluster.vpnet_serverless.cluster_id
-    kubeconfig          = alicloud_cs_serverless_kubernetes_cluster.vpnet_serverless.kubeconfig
+    api_server_endpoint = alicloud_cs_managed_kubernetes.vpnet_serverless.endpoint
+    cluster_id          = alicloud_cs_managed_kubernetes.vpnet_serverless.id
+    kubeconfig          = alicloud_cs_managed_kubernetes.vpnet_serverless.kube_config_raw
   }
 }
