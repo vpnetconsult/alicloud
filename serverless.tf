3a0611eb620835b8cedf20d0825df59bb5c61eb4
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 13:25:32 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,10 +1,10 @@
 
-resource "alicloud_oss_bucket" "demo" {
-   bucket = "demo"
+resource "alicloud_oss_bucket" "vpnet_demo" {
+   bucket = "vpnet_demo"
 }
 
 resource "alicloud_oss_bucket_object" "demo" {
-   bucket = alicloud_oss_bucket.demo.bucket
+   bucket = alicloud_oss_bucket.vpnet_demo.bucket
    key = "function/demo.zip"
    source = "demo.zip"
 }
