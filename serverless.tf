696e1d35e5a986b3bd153dddd51b7f89f134e6fe
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 13:09:51 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,18 +1,12 @@
 
 resource "alicloud_fc_service" "vpnet_service" {
-  service_name = "test"
   description  = "My Function Compute service"
 }
 
 resource "alicloud_fc_function" "vpnet_function" {
-  function_name    = "vpnet-function"
-  service_name     = alicloud_fc_service.vpnet_service.test
   handler          = "index.handler"
   runtime          = "nodejs10"
   memory_size      = 128
   timeout          = 3
-  environment_vars = {
-    API_URL = "https://vpnet.app/api"
-  }
 }
 
