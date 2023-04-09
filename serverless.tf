1f39a4ce9d3b7d084070bf6795a10afe4fa2d896
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 15:15:25 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,22 +1,22 @@
-resource "alicloud_cs_kubernetes" "k8s" {
-  count                 = 1
-  master_vswitch_ids    = length(var.vswitch_ids) > 0 ? split(",", join(",", var.vswitch_ids)) : length(var.vswitch_cidrs) < 1 ? [] : split(",", join(",", alicloud_vswitch.vswitches.*.id))
-  master_instance_types = var.master_instance_types
-  node_cidr_mask        = var.node_cidr_mask
-  enable_ssh            = var.enable_ssh
-  install_cloud_monitor = var.install_cloud_monitor
-  proxy_mode            = var.proxy_mode
-  password              = var.password
-  pod_cidr              = var.pod_cidr
-  service_cidr          = var.service_cidr
-  # version can not be defined in variables.tf.
-  version = "1.24.6-aliyun.1"
-  dynamic "addons" {
-    for_each = var.cluster_addons
-    content {
-      name     = lookup(addons.value, "name", var.cluster_addons)
-      config   = lookup(addons.value, "config", var.cluster_addons)
-      disabled = lookup(addons.value, "disabled", var.cluster_addons)
-    }
+resource "alicloud_cs_serverless_kubernetes" "serverless" {
+  name_prefix                    = var.name
+  vpc_id                         = alicloud_vpc.vpc.id
+  vswitch_ids                    = [alicloud_vswitch.vswitch.id]
+  new_nat_gateway                = true
+  endpoint_public_access_enabled = true
+  deletion_protection            = false
+
+  load_balancer_spec      = "slb.s2.small"
+  time_zone               = local.zone
+  service_cidr            = "172.21.0.0/20"
+  service_discovery_types = ["PrivateZone"]
+  # Enable log service, A project named k8s-log-{ClusterID} will be automatically created
+  logging_type = "SLS"
+  # Select an existing sls project
+  # sls_project_name             = ""
+
+  # tags 
+  tags = {
+    "k-aa" = "v-aa"
+    "k-bb" = "v-aa"
   }
-}
