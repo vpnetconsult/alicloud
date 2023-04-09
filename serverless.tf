e7dbcd4a46ed81a0eca12e0968146d178423ec2b
eoskl <42143496+eoskl@users.noreply.github.com>
2023-04-09 11:34:25 +0800

    Update serverless.tf

--- serverless.tf
+++ serverless.tf
@@ -1,32 +1,36 @@
-# Use the kubernetes module to create a serverless Kubernetes cluster
-module "vpnet_serverless" {
-  source  = "terraform-alicloud-modules/kubernetes/alicloud"
-  version = "3.3.0"
-
-  cluster_name            = "vpnet_serverless"
-  region_id               = local.zone
-  zone_ids                = [local.zone]
-  vpc_id                  = alicloud_vpc.vpc.id
-  vswitch_ids             = [alicloud_vswitch.vswitch.id]
-  nat_gateway             = true
-  num_of_nodes            = 3
-  kubernetes_version      = "1.21.2-aliyun.1"
-  enable_auto_renew       = true
-  enable_dashboard        = true
-  enable_nginx_ingress    = true
-  enable_log_service      = true
-  enable_private_zone     = true
-  enable_private_endpoint = true
-  tags                    = {
-    Environment = "production"
-  }
+# Create a Serverless Function Compute service
+resource "alicloud_fc_service" "serverless_service" {
+  name        = "vpnet-serverless-service"
+  description = "My Serverless Service"
+  region      = local.zone
+  vpc_id      = alicloud_vpc.vpc.id
 }
 
-# Output the Kubernetes cluster and Load Balancer details
-output "cluster_details" {
-  value = {
-    api_server_endpoint = alicloud_cs_managed_kubernetes.vpnet_serverless.endpoint
-    cluster_id          = alicloud_cs_managed_kubernetes.vpnet_serverless.id
-    kubeconfig          = alicloud_cs_managed_kubernetes.vpnet_serverless.kube_config_raw
-  }
+# Create a Serverless Function Compute function
+resource "alicloud_fc_function" "serverless_function" {
+  service_name = alicloud_fc_service.serverless_service.name
+  name         = "vpnet-serverless-function"
+  description  = "My Serverless Function"
+  runtime      = "nodejs12"
+  handler      = "index.handler"
+  code_filename = "serverless.zip"
+  memory_size  = 128
+  timeout      = 60
+}
+
+# Create an API Gateway resource for routing HTTP requests to the function
+resource "alicloud_api_gateway" "api_gateway" {
+  name = "vpnet-api-gateway"
+  protocol = "HTTP"
+  instance_id = alicloud_fc_service.serverless_service.id
+  description = "My API Gateway"
+  api_group_name = "vpnet-api-group"
+  api_name = "vpnet-api"
+  method = "GET"
+  request_path = "/vpnet-api"
+}
+
+# Output the API Gateway URL
+output "api_gateway_url" {
+  value = alicloud_api_gateway.api_gateway.invoke_url
 }
